b082566c92ecc36d236d2028d29310fba63c4729
fuwafuwa <ytx070109@gmail.com>
2021-06-21 21:37:26 +0800

    C语言实现全排列问题的一般方法

--- testcode_2021_6_21c/猜名次.c
+++ testcode_2021_6_21c/猜名次.c
@@ -1,28 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 //5位运动员参加了10米台跳水比赛，有人让他们预测比赛结果：
-
+//
 //A选手说：B第二，我第三；
 //B选手说：我第二，E第四；
 //C选手说：我第一，D第二；
 //D选手说：C最后，我第三；
 //E选手说：我第四，A第一；
-
+//
 //比赛结束后，每位选手都说对了一半，请编程确定比赛的名次。
 
 #include <stdio.h>
 
-void swap(int* a, int* b)
+void exchange(int* a, int* b)//exchange函数 交换两个变量的值
 {
     int c = *a;
     *a = *b;
     *b = c;
 }
 
-void perm(int str[], int index, int str_size)
+void perm(int str[], int start, int str_size)//str传入全排列的对象 start全排列的起点 str_size是元素个数
 {
     int i = 0, j = 0;
 
-    if (index == str_size)
+    if (start == str_size)//start == str_size 意味着 上一轮中的start = str_size - 1 也就是说遍历到头了 跳出递归
     {
         /* 输出当前的排列 */
         if (((str[1] == 2 && str[0] != 3) || (str[1] != 2 && str[0] == 3))
@@ -34,13 +34,14 @@ void perm(int str[], int index, int str_size)
     }
     else
     {
-        for (j = index; j < str_size; j++)
+        for (j = start; j < str_size; j++)
         {
-            swap(&str[j], &str[index]);
-            perm(str, index + 1, str_size);
-            swap(&str[j], &str[index]);
+            exchange(&str[j], &str[start]);//依次把每个元素放到第一
+            perm(str, start + 1, str_size);//让剩下的元素全排列
+            exchange(&str[j], &str[start]);//再把第一的元素还原回去 还原数组顺序
         }
     }
+    return;
 }
 int main()
 {
